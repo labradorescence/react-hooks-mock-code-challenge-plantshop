b40ec3c6d551a5ed474911a8acf3e79fed63a893
labradorescence <orangenewyorkcreativeagency@gmail.com>
2023-12-20 15:22:14 -0500

    review

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard( { url, plant, onHandleDelete }) {
+
+  const { id, name, image, price } = plant
+
+  const [ isInStock, setIsInStock ] = useState(true)
+  const [ showEditForm, setShowEditForm ] = useState(false)
+  const [ newPrice, setNewPrice ] = useState(price)
+
+  const handleClick = () => {
+    setIsInStock(!isInStock)
+  }
+
+  const handleChange = (e) => {
+    setNewPrice(e.target.value)
+  }
+
+  const handleEdit = () => {
+    setShowEditForm(!showEditForm)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    fetch(`${url}/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({ price: newPrice})
+    })
+    .then(response => response.json())
+    .then(newlyUpdatedPlantObj => console.log(newlyUpdatedPlantObj))
+    .catch(console.error)
+  }
+
+  const handleDelete = () => {
+
+    fetch(`${url}/${id}`, {
+      method: "DELETE"
+    })
+      .then(response=>response.json())
+      .then(console.log("deleted"))
+      .catch(console.error)
+
+      onHandleDelete(id)
+  }
+
   return (
     <li className="card">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+
+      {showEditForm?  <form onSubmit = {handleSubmit}> 
+        <label>
+          new price:
+          <input name="price" value={newPrice} onChange={handleChange} />
+        </label>
+        <button type="submit">submit</button>
+      </form> : <p>Price: {price}</p>}
+      <button onClick={handleEdit}>edit</button> 
+      <button onClick={handleDelete}>delete</button>
+      {isInStock ? (
+        <button onClick={handleClick} className="primary">In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleClick} >Out of Stock</button>
       )}
     </li>
   );
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
+const url = "http://localhost:6001/plants"
 
 function PlantPage() {
+
+  const [ plantsArr, setPlantsArr] = useState([])
+  const [ searchVal, setSearchVal ] = useState("")
+
+
+  useEffect(() => {
+    fetch(url)
+    .then(response=>response.json())
+    .then(data => setPlantsArr(data))
+    .catch(console.error)
+  }, [])
+
+ // console.log(plantsArr)
+
+  const searchedPlantArr = plantsArr.filter(eachPlant => {
+    return eachPlant.name.toLowerCase().includes(searchVal.toLowerCase())
+  })
+
+  //console.log(searchedPlantArr)
+
+  const addNewPlant = (newlyAddedPlant) => {
+    setPlantsArr([...plantsArr, newlyAddedPlant])
+  }
+
+  const handleSearch = (e) => {
+    setSearchVal(e.target.value)
+  }
+
+
+
+  const handleDelete = (deletedPlantId) => {
+    const plantsArrWOdelted = plantsArr.filter(eachPlant => {
+      return eachPlant.id !== deletedPlantId
+    }) 
+   setPlantsArr(plantsArrWOdelted)
+  } 
+
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm url={url} onAddNewPlant={addNewPlant}/>
+      <Search onSearch={handleSearch}/>
+      <PlantList plantsArr={searchedPlantArr} url={url} onHandleDelete={handleDelete}/>
     </main>
   );
 }
